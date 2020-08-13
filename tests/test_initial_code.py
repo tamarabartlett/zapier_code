c0c7fbeee1f3b0cbe43d88333aed63a25f6f9f16
Tamara Bartlett <tamara.bartlett@uplight.com>
2020-08-13 16:51:35 -0600

    Gets 1mb chunk

--- tests/test_initial_code.py
+++ tests/test_initial_code.py
@@ -1,7 +1,24 @@
+import os
+
 from unittest import TestCase
-from memcached_lib.initial_code import file_less_than_50mb
+from memcached_lib.initial_code import (
+    file_less_than_50mb,
+    get_one_mb_of_file,
+)
 
 class InitialCodeTests(TestCase):
+
+    def test_get_first_mb_is_one_mb(self):
+        file_name = 'two_mb_file.txt'
+        with open(file_name, 'wb') as f:
+            one_mb = 1024 * 1024
+            two_mb = one_mb * 2
+            f.write(b'0' * two_mb)
+
+            returned_file = get_one_mb_of_file(file_name)
+            file_size = os.path.getsize('two_mb_file.txt_1')
+        self.assertEqual(one_mb, file_size)
+
 
     def test_file_less_than_50mb_is_true(self):
         file_name = "small_file.txt"
